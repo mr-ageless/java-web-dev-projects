00aca91dcc3cb3cafbaffab9b42e85c42edcf5b3
Joe Minor <joeminor8533@gmail.com>
2023-09-14 20:28:01 -0500

    Studio 6: unfinished

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -6,5 +6,60 @@ public class Student {
 
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
+//public String getName() {
+//        return name;
+//}
+//public void setName(String aName) {
+//        name = aName;
+//}
+//public int getStudentId() {
+//        return studentId;
+//}
+//public void setStudentId(Integer aStudentId) {
+//        studentId = aStudentId;
+//}
+//public int getNumberOfCredits() {
+//        return numberOfCredits;
+//}
+//public void setName(Integer aNumberOfCredits) {
+//        numberOfCredits = aNumberOfCredits;
+//}
+//public double getGpa() {
+//        return gpa;
+//}
+//public void setGpa(double Gpa) {
+//        this.gpa = gpa;
+//}
+        public void setName(String name) {
+                this.name = name;
+        }
 
+        public void setStudentId(int studentId) {
+                this.studentId = studentId;
+        }
+
+        public void setGpa(double gpa) {
+                this.gpa = gpa;
+        }
+
+        private void setNumberOfCredits(int numberOfCredits) {
+                this.numberOfCredits = numberOfCredits;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public int getStudentId() {
+                return studentId;
+        }
+
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        }
+
+        public double getGpa() {
+                return gpa;
+        }
 }
+
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,9 @@
+import javax.naming.Name;
+
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        //instantiate your Student class below
+        Student student = new Student("Joe", 12345, 1, 4.0);
+
     }
 }
